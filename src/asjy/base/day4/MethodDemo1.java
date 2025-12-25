37280ef014fce589775da3d6d5f5b9bad39a01cb
jeffsui <pinghailinfeng79@gmail.com>
2025-12-25 10:38:58 +0800

    add method demo

---
+++ src/asjy/base/day4/MethodDemo1.java
@@ -0,0 +1,13 @@
+package asjy.base.day4;
+
+public class MethodDemo1 {
+    public static void hello(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        /**
+         * 功能封装
+          */
+        hello();
+    }
+}
